35920fb26b2d17a823deb9184559608913056908
Kirill <kirikvin8490@gmail.com>
2026-02-20 22:03:38 +0300

    Add flags and second cow

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -1,4 +1,5 @@
 import argparse
+import cowsay
 
 parser = argparse.ArgumentParser()
 
@@ -15,5 +16,19 @@ parser.add_argument("-F", dest="cow2", default="default")
 
 args = parser.parse_args()
 
-print(args)
+cow1 = cowsay.cowsay(args.message1, eyes=args.eyes1, tongue=args.tongue1, cow=args.cow1)
+cow2 = cowsay.cowsay(args.message2, eyes=args.eyes2, tongue=args.tongue2, cow=args.cow2)
+
+cow1_lines = cow1.splitlines()
+cow2_lines = cow2.splitlines()
+
+w1 = max((len(s) for s in cow1_lines), default=0)
+w2 = max((len(s) for s in cow2_lines), default=0)
+h = max(len(cow1_lines), len(cow2_lines))
+
+cow1_pad = [" " * w1] * (h - len(cow1_lines)) + cow1_lines
+cow2_pad = [" " * w2] * (h - len(cow2_lines)) + cow2_lines
+
+for l1, l2 in zip(cow1_pad, cow2_pad):
+    print(l1.ljust(w1) + " " + l2)
 
